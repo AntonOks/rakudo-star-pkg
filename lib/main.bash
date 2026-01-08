df76ae3ee353960129b16d9bcd4abf81e2c7c012
AntonOks <notna@ist-einmalig.de>
2026-01-08 21:38:50 +0100

    everything works ok.
    further improvements to be done...

--- lib/main.bash
+++ lib/main.bash
@@ -14,7 +14,9 @@ RSTARPKGR_TOOLS+=(
   gcc
   curl
   tar
+  tput
   nfpm
+  envsubst
 )
 
 main() {
@@ -34,8 +36,8 @@ main() {
   local RSTARPKGR_BACKEND="moar"
   local RSTARPKGR_VERSION="${RSTARPKGR_VERSION:-"latest"}"
   local RSTARPKGR_REVISION="${RSTARPKGR_REVISION:-"01"}"
-  local RSTARPKGR_OS="${RSTARPKGR_OS:-"linux"}"				# uname -s | awk '{print tolower($0)}'
-  local RSTARPKGR_ARCH="${RSTARPKGR_ARCH:-"x86_64"}"		# uname -m
+  local RSTARPKGR_OS="${RSTARPKGR_OS:-"linux"}"              # uname -s | awk '{print tolower($0)}'
+  local RSTARPKGR_ARCH="${RSTARPKGR_ARCH:-"x86_64"}"         # uname -m
   local RSTARPKGR_TOOLCHAIN="${RSTARPKGR_TOOLCHAIN:-"gcc"}"
 
   # Rakudo-Star `rstar` release examples, to build an own binary release from:
@@ -63,53 +65,94 @@ main() {
   done
   shift $(( OPTIND - 1 ))
 
-  if [ $RSTARPKGR_GETOPT_ERROR ]; then usage ; exit; fi
+  if [ ${RSTARPKGR_GETOPT_ERROR} ]; then usage ; exit 3; fi
+
+  export RSTAR_DEBUG=${RSTARPKGR_DEBUG}
 
   # Maintain our own tempdir
-  mkdir -p -- "$RSTARPKGR_TMPDIR"
-  debug "\$RSTARPKGR_TMPDIR set to $RSTARPKGR_TMPDIR"
-
+  if ! [[ -d ${RSTARPKGR_TMPDIR} ]]; then mkdir -p -- "${RSTARPKGR_TMPDIR}"; fi
+  debug "\"\$RSTARPKGR_TMPDIR\" set to \"${RSTARPKGR_TMPDIR}\""
 
   # Get the Rakudo-Star source
-  if [ $RSTARPKGR_VERSION == "latest" ]; then
-    debug "Fetching Rakudo-Star source from https://rakudo.org/latest/star/src"
+  if [[ "${RSTARPKGR_VERSION}" == "latest" ]]; then
+    debug "Fetching Rakudo-Star source from \"https://rakudo.org/latest/star/src\""
     curl -LsS https://rakudo.org/latest/star/src -o ${RSTARPKGR_TMPDIR}/rakudo-star.tar.gz
   else
-    debug "Fetching Rakudo-Star source from https://www.rakudo.org/dl/star/rakudo-star-${RSTARPKGR_VERSION}-${RSTARPKGR_REVISION}.tar.gz"
+    # TODO: check if the Rakudo release was already downloaded before and still exists...
+    #       if so, we can skip plenty of steps
+    debug "Fetching Rakudo-Star source from \"https://www.rakudo.org/dl/star/rakudo-star-${RSTARPKGR_VERSION}-${RSTARPKGR_REVISION}.tar.gz\""
     if [[ $(curl -LsS https://www.rakudo.org/dl/star/rakudo-star-${RSTARPKGR_VERSION}-${RSTARPKGR_REVISION}.tar.gz -o ${RSTARPKGR_TMPDIR}/rakudo-star.tar.gz) ]]; then
 	    crit "Couldn't download https://www.rakudo.org/dl/star/rakudo-star-${RSTARPKGR_VERSION}-${RSTARPKGR_REVISION}.tar.gz"
-	    crit "You may need to verify your \"$RSTARPKGR_VERSION\" and or \"$RSTARPKGR_REVISION\" system variables or command line options."
+	    crit "You may need to verify your \$RSTARPKGR_VERSION (\"${RSTARPKGR_VERSION}\") and \$RSTARPKGR_REVISION (\"${RSTARPKGR_REVISION}\") system variables"
+      crit "or"
+      crit "your command line options [-V] and [-R]."
 	    exit 5
 	  fi
   fi
 
+
+  # get the downloaded Rakudo-Star sources into the right directory structure
+  cd ${RSTARPKGR_TMPDIR}
+  mkdir -p -- ${RSTARPKGR_TMPDIR}/rstar_src && tar -xzf rakudo-star.tar.gz --directory ${RSTARPKGR_TMPDIR}/rstar_src && rm -f -- rakudo-star.tar.gz
+  if [[ ${RSTARPKGR_VERSION} == "latest" ]]; then
+	  RSTARPKGR_VERSION="$(ls -d ${RSTARPKGR_TMPDIR}/rstar_src/rakudo-star-* | grep -Po "(\d+\.\d+)(\\.[0-9]+)?")"
+    debug "Changed \$RSTARPKGR_VERSION from \"latest\" to \"${RSTARPKGR_VERSION}\""
+  fi
+  
+  mv -- ${RSTARPKGR_TMPDIR}/rstar_src ${RSTARPKGR_TMPDIR}/rakudo-star-${RSTARPKGR_VERSION}-${RSTARPKGR_REVISION}_src && cd ${RSTARPKGR_TMPDIR}/rakudo-star-${RSTARPKGR_VERSION}-${RSTARPKGR_REVISION}_src
+  mv -f -- rakudo-star-*/* .
+  rm -fr -- rakudo-star-*
   cd ${RSTARPKGR_TMPDIR}
 
-  mkdir -p -- rstar_src && tar -xzf rakudo-star.tar.gz --directory rstar_src && rm -f rakudo-star.tar.gz && cd rstar_src
-  if [ $RSTARPKGR_VERSION == "latest" ]; then
-	  RSTARPKGR_VERSION="$(ls -d rakudo-star-* | grep -Po "(\d+\.\d+)(\\.[0-9]+)?")"
-    debug "Changing \$RSTARPKGR_VERSION from \"latest\" to \"$RSTARPKGR_VERSION\""
-  fi
-  mv rakudo-star-*/* .
-  rm -fr rakudo-star-* && cd ..
+  # enforce some directories
+  if ! [[ -d ${RSTARPKGR_BASEDIR}/pkgs/ ]]; then mkdir -p -- ${RSTARPKGR_BASEDIR}/pkgs/; fi
+  if ! [[ -d ${RSTARPKGR_TMPDIR}/rakudo-star-${RSTARPKGR_VERSION}-${RSTARPKGR_REVISION}_bin/ ]]; then mkdir -p -- ${RSTARPKGR_TMPDIR}/rakudo-star-${RSTARPKGR_VERSION}-${RSTARPKGR_REVISION}_bin/; fi
 
-  export RSTAR_DEBUG=$RSTARPKGR_DEBUG
-  debug "Building Rakudo-Star with \"./rstar_src/bin/rstar install -p $(pwd -P)/rstar_bin\""
-  echo "LINE 104: running \"./rstar_src/bin/rstar install -p $(pwd -P)/rstar_bin\""
+  # we use the Rakudo-Star `rstar` bash tool to compile and install Rakudo-Star
+  # we expect the file `etc/fetch_core.txt` of the dowloaded $RSTARPKGR_VERSION
+  #  is correct as it will be used by the `rstar` tool internally
+  debug "Building Rakudo-Star with \"./rakudo-star-${RSTARPKGR_VERSION}-${RSTARPKGR_REVISION}_src/bin/rstar install -p ${RSTARPKGR_TMPDIR}/rakudo-star-${RSTARPKGR_VERSION}-${RSTARPKGR_REVISION}_bin\""
+  if [[ "${RSTARPKGR_DEBUG}" ]]; then
+    ./rakudo-star-${RSTARPKGR_VERSION}-${RSTARPKGR_REVISION}_src/bin/rstar install -p ${RSTARPKGR_TMPDIR}/rakudo-star-${RSTARPKGR_VERSION}-${RSTARPKGR_REVISION}_bin
+  else
+    ./rakudo-star-${RSTARPKGR_VERSION}-${RSTARPKGR_REVISION}_src/bin/rstar install -p ${RSTARPKGR_TMPDIR}/rakudo-star-${RSTARPKGR_VERSION}-${RSTARPKGR_REVISION}_bin > /dev/null 2>&1
+  fi
+
+  if [[ $? == "0" ]]; then
+    debug "Creating the \"rakudo-star-linux-relocable-${RSTARPKGR_VERSION}-${RSTARPKGR_REVISION}_${RSTARPKGR_ARCH}.tar.gz\""
+    tar -czf ${RSTARPKGR_BASEDIR}/pkgs/rakudo-star-linux-relocable-${RSTARPKGR_VERSION}-${RSTARPKGR_REVISION}_${RSTARPKGR_ARCH}.tar.gz -C ${RSTARPKGR_TMPDIR}/rakudo-star-${RSTARPKGR_VERSION}-${RSTARPKGR_REVISION}_bin/ .
+  else
+    crit "\"rstar\" couldn't install Rakudo, please investigate why..."
+    exit 10
+  fi
+
+  # export the variables we use in the `nfpm.yaml_temp` template file
+  if [[ ${RSTARPKGR_ARCH} == "x86_64" ]]; then RSTARPKGR_ARCH="amd64"; fi
+  export RSTARPKGR_VERSION RSTARPKGR_REVISION RSTARPKGR_ARCH RSTARPKGR_OS
+  debug "Variables we use with nfpm are: RSTARPKGR_VERSION=\"${RSTARPKGR_VERSION}\", RSTARPKGR_REVISION=\"${RSTARPKGR_REVISION}\", RSTARPKGR_ARCH=\"${RSTARPKGR_ARCH}\", RSTARPKGR_OS=\"${RSTARPKGR_OS}\""
   
-  # export the variables we use in the nfpm_vars.yaml file
-  if [ $RSTARPKGR_ARCH == "x86_64" ]; then RSTARPKGR_ARCH="amd64"; fi
-    export RSTARPKGR_VERSION RSTARPKGR_REVISION RSTARPKGR_ARCH RSTARPKGR_OS
-    debug "Variables used with nfpm are: RSTARPKGR_VERSION=\"$RSTARPKGR_VERSION\", RSTARPKGR_REVISION=\"$RSTARPKGR_REVISION\", RSTARPKGR_ARCH=\"$RSTARPKGR_ARCH\", RSTARPKGR_OS=\"$RSTARPKGR_OS\""
-    for PKG in apk archlinux deb rpm; do
-      nfpm pkg --config $RSTARPKGR_BASEDIR/etc/nfpm.yaml --packager $PKG --target $RSTARPKGR_BASEDIR/pkgs/
-    done
-  cd $RSTARPKGR_BASEDIR
+  # seems like nfpm cannot handle a combination of variables properly
+  # we work arround with `envsubst`
+  envsubst < ${RSTARPKGR_BASEDIR}/etc/nfpm.yaml_temp > ${RSTARPKGR_BASEDIR}/etc/nfpm.yaml 
+  
+  # now let's build the packages
+  debug "Building \"apk archlinux deb rpm\" packages with \"nfpm\"."
+  for PKG in apk archlinux deb rpm; do
+    if [[ "${RSTARPKGR_DEBUG}" ]]; then
+      nfpm pkg --config ${RSTARPKGR_BASEDIR}/etc/nfpm.yaml --packager $PKG --target ${RSTARPKGR_BASEDIR}/pkgs/
+    else
+      nfpm pkg --config ${RSTARPKGR_BASEDIR}/etc/nfpm.yaml --packager $PKG --target ${RSTARPKGR_BASEDIR}/pkgs/ > /dev/null
+    fi
+  done
+  cd ${RSTARPKGR_BASEDIR}
 
   # Clean up if necessary
-  if [[ -z $RSTARPKGR_CLEANUP ]]; then
-    debug "Cleaning up temp. dir \"$RSTARPKGR_TMPDIR\""
-    rm -rf -- "$RSTARPKGR_TMPDIR"
+  if [[ -z ${RSTARPKGR_CLEANUP} ]]; then
+    debug "Removing temp dir \"${RSTARPKGR_TMPDIR}\""
+    rm -rf -- "${RSTARPKGR_TMPDIR}"
+
+    debug "Cleaning up files in pkgs dir \"${RSTARPKGR_BASEDIR}/pkgs/\""
+    rm -f -- "${RSTARPKGR_BASEDIR}/pkgs/*"
   fi
 
 }
@@ -136,10 +179,21 @@ Options:
   -d                    Debug mode. Plenty of additional informations.
 
 Environment variables:
-	RSTARPKGR_DEBUG
-		            Show more (debug) informations.
-	GPG_FINGERPRINT
-                	The fingerprint of the key to use for signing release files.
+  RSTARPKGR_BACKEND     Rakudo backend, only "moar" is supported by Rakudo.
+                        DEFAULT="moar"
+  RSTARPKGR_VERSION     Rakudo-Star version to build pkgs for.
+                        DEFAULT="latest"
+  RSTARPKGR_REVISION    Rakudo-Star revisions.
+                        DEFAULT="01"
+  RSTARPKGR_OS          DEFAULT="linux"     # uname -s | awk '{print tolower(\$0)}'
+  RSTARPKGR_ARCH        DEFAULT="x86_64"    # uname -m
+  RSTARPKGR_TOOLCHAIN   On Linux it's allways gcc.
+  RSTARPKGR_TMPDIR      Temp. directory.
+                        DEAULT="./tmp"
+  RSTARPKGR_CLEANUP     Cleanup $RSTARPKGR_TMPDIR
+  RSTARPKGR_DEBUG       Show more (debug) informations.
+                        DEFAULT="0"
+  GPG_FINGERPRINT       The fingerprint of the key to use for signing release files.
 
 EOF
 }
